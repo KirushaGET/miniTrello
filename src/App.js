ae0c5fe529656bdc9c54829468d5fc250ca3193c
Kirill <kirill-garnov@mail.ru>
2021-07-05 18:26:03 +0300

    added dragging and dropping columns

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.scss';
 const App = () => {
   const [allTasks, setAllTasks] = useState([]);
   const [columnName, setColumnName] = useState("");
+  const [currentColumn, setCurrentColumn] = useState({});
 
   const newColumn = () => {
     const newColumnValue = {
@@ -14,6 +15,29 @@ const App = () => {
 
     setAllTasks((prev) => [...prev, newColumnValue]);
     setColumnName("");
+  }
+
+  const dragStartHandler = (e, column, index) => {
+    setCurrentColumn({value: column, index});
+  }
+
+  const dropHandler = (e, column) => {
+    e.preventDefault();
+    const tempArray = [...allTasks];
+    tempArray.splice(currentColumn.index, 1);
+    const indexNewColumn = allTasks.indexOf(column);
+    tempArray.splice(indexNewColumn, 0, currentColumn.value);
+    setAllTasks(tempArray);
+    e.target.style.background = '#f0f0f0';
+  }
+
+  const dropOverHandler = (e) => {
+    e.preventDefault();
+    e.target.style.background = 'grey';
+  }
+
+  const dragEndHandler = (e) => {
+    e.target.style.background = '#f0f0f0';
   }
 
   return (
@@ -29,7 +53,15 @@ const App = () => {
       </div>
       <div className="app__body">
         {allTasks.map((column, index) => (
-          <div key={`${column.name}-${index}`}>
+          <div 
+            key={`${column.name}-${index}`} 
+            draggable
+            onDragStart={(e) => dragStartHandler(e, column, index)}
+            onDrop={(e) => dropHandler(e, column)}
+            onDragOver={(e) => dropOverHandler(e)}
+            onDragLeave={(e) => dragEndHandler(e)}
+            onDragEnd={(e) => dragEndHandler(e)}
+          >
             <Column 
               column={column} 
               allTasks={allTasks} 
